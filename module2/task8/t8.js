3547500523bdfaafc28b2c76f6ae1d2eaa35ae2f
dipak <dipakparmar079@gmail.com>
2024-11-13 18:01:26 +0200

    module2

---
+++ module2/task8/t8.js
@@ -0,0 +1,16 @@
+function concat(arr) {
+    let result = "";
+
+    for (let i = 0; i < arr.length; i++) {
+      result += arr[i];
+    }
+
+    return result;
+  }
+
+  const names = ["Johnny", "DeeDee", "Joey", "Marky"];
+
+
+  let concatenatedString = concat(names);
+
+  document.getElementById("result").innerHTML = concatenatedString;
